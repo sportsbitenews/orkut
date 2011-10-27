78d71a4ec66daad21386459ca41d60e850e98a64
yTrooper <ygor@umanni.com.br>
2011-10-27 20:04:30 -0200

    tests.

--- lib/orkut/client/comments.rb
+++ lib/orkut/client/comments.rb
@@ -19,12 +19,12 @@ module Orkut
             Orkut::Constants::Fields::USER_ID     => Orkut::Constants::InternalConstants::USERID_ME,
             Orkut::Constants::Fields::COUNT       => (options[:count] || Orkut::Client::Base::DEFAULT_COUNT),
             Orkut::Constants::Fields::START_INDEX => 0,
-            Orkut::Constants::Fields::ACTIVITY_ID => options[:activity_id]
+            Orkut::Constants::Fields::ACTIVITY_ID => options[:activity_id],
+            Orkut::Constants::Fields::TYPE        => Orkut::Constants::Fields::ACTIVITIES
           },
           Orkut::Constants::Fields::ID     => request_id.to_s+'-'+Orkut::Constants::MethodNames::COMMENTS_GET,
           Orkut::Constants::Fields::METHOD => Orkut::Constants::MethodNames::COMMENTS_GET
         }]
-#            Orkut::Constants::Fields::TYPE        => Orkut::Constants::Fields::ACTIVITIES,
         MultiJson.decode(post(nil, params.to_s, default_headers).body)
       end
     end
--- lib/orkut/client/timelines.rb
+++ lib/orkut/client/timelines.rb
@@ -45,6 +45,25 @@ module Orkut
         }]
         MultiJson.decode(post(nil, params.to_s, default_headers).body)
       end
+      
+      def home_timeline22(options={})
+        params = [{
+          Orkut::Constants::Fields::PARAMS => {
+            Orkut::Constants::Fields::FIELDS => ['comments'],
+            Orkut::Constants::Fields::GROUP_ID    => Orkut::Constants::Group::ALL,
+            Orkut::Constants::Fields::USER_ID     => Orkut::Constants::InternalConstants::USERID_ME,
+            Orkut::Constants::Fields::COUNT       => (options[:count] || Orkut::Client::Base::DEFAULT_COUNT),
+            Orkut::Constants::Fields::COALESCE    => true,
+            Orkut::Constants::Fields::START_INDEX => 0
+          },
+          Orkut::Constants::Fields::ID     => request_id.to_s+'-'+Orkut::Constants::MethodNames::ACTIVITIES_GET,
+          Orkut::Constants::Fields::METHOD => Orkut::Constants::MethodNames::ACTIVITIES_GET
+        }]
+        unless options[:updated_before].blank?
+          params.first[Orkut::Constants::Fields::PARAMS][Orkut::Constants::Fields::UPDATED_BEFORE] = format_datetime(options[:updated_before])
+        end
+        MultiJson.decode(post(nil, params.to_s, default_headers).body)
+      end
     end
   end
 end
