f2da68245f7cbcd64d5109f1318cf945711e56de
yTrooper <ygor@umanni.com.br>
2011-11-01 11:19:19 -0200

    Add new features. new gem version.

--- lib/orkut/connection.rb
+++ lib/orkut/connection.rb
@@ -3,48 +3,6 @@ require 'google/api_client'
 
 module Orkut
   module Connection
-  private
-
-    def connection_v1(options={})
-      access_token = nil
-      if authenticated?
-        client_options = {
-          :site => site,
-          :request_token_path => request_token_path,
-          :access_token_path => access_token_path,
-          :authorize_path => authorize_path
-        }
-        consumer = ::OAuth::Consumer.new(consumer_key, consumer_secret, client_options)
-        # now create the access token object from passed values
-        token_hash = { :oauth_token => oauth_token,
-          :oauth_token_secret => oauth_token_secret
-        }
-        access_token = ::OAuth::AccessToken.from_hash(consumer, token_hash)
-      end
-      access_token
-    end
-
-    def connection(options={})
-      access_token = nil
-      if authenticated?
-        client = Google::APIClient.new
-
-        client.authorization.client_id     = client_id
-        client.authorization.client_secret = client_secret
-        client.authorization.scope         = scope
-        client.authorization.redirect_uri  = redirect_uri
-
-        client.authorization.code          = code
-
-        orkut = client.discovered_api('orkut', 'v2')
-        unless client.authorization.access_token
-          r = Rack::Response.new
-          r.redirect(client.authorization.authorization_uri.to_s)
-          r.finish
-        end
-      end
-      access_token
-    end
 
     def authorization_uri(options={})
       client = Google::APIClient.new
@@ -58,6 +16,48 @@ module Orkut
 
       client.authorization.authorization_uri.to_s
     end
-    
+
+    private
+
+      def connection_v1(options={})
+        access_token = nil
+        if authenticated?
+          client_options = {
+            :site => site,
+            :request_token_path => request_token_path,
+            :access_token_path => access_token_path,
+            :authorize_path => authorize_path
+          }
+          consumer = ::OAuth::Consumer.new(consumer_key, consumer_secret, client_options)
+          # now create the access token object from passed values
+          token_hash = { :oauth_token => oauth_token,
+            :oauth_token_secret => oauth_token_secret
+          }
+          access_token = ::OAuth::AccessToken.from_hash(consumer, token_hash)
+        end
+        access_token
+      end
+
+      def connection(options={})
+        access_token = nil
+        if authenticated?
+          client = Google::APIClient.new
+
+          client.authorization.client_id     = client_id
+          client.authorization.client_secret = client_secret
+          client.authorization.scope         = scope
+          client.authorization.redirect_uri  = redirect_uri
+
+          client.authorization.code          = code
+
+          orkut = client.discovered_api('orkut', 'v2')
+          unless client.authorization.access_token
+            r = Rack::Response.new
+            r.redirect(client.authorization.authorization_uri.to_s)
+            r.finish
+          end
+        end
+        access_token
+      end
   end
 end
--- lib/orkut/version.rb
+++ lib/orkut/version.rb
@@ -14,7 +14,7 @@ module Orkut
     end
 
     def self.pre
-      9 #nil
+      10 #nil
     end
 
     def self.to_s
