09a2e4e81fb2f0aad9476966faf1e701adad51b1
yTrooper <ygor@umanni.com.br>
2011-11-03 14:31:53 -0200

    Add new features.

--- lib/orkut/client/comments.rb
+++ lib/orkut/client/comments.rb
@@ -13,7 +13,17 @@ module Orkut
       # TODO implements features
 
       def get_comments(options={})
-
+        params = {
+          Orkut::Constants::Fields::USER_ID      => Orkut::Constants::InternalConstants::USERID_ME_SIMPLE,
+          Orkut::Constants::Fields::ACTIVITY_ID  => options[:activity_id],
+          Orkut::Constants::Fields::ALT          => Orkut::Constants::Params::JSON,
+          Orkut::Constants::Fields::MAX_RESULTS  => (options[:count] || Orkut::Client::Base::DEFAULT_COUNT).to_s,
+          Orkut::Constants::Fields::EXTRA_PARAMS => options
+        }
+        unless options[:page_token].blank?
+          params[Orkut::Constants::Fields::PAGE_TOKEN] = options[:page_token]
+        end
+        MultiJson.decode(execute(Orkut::Constants::Fields::COMMENTS, Orkut::Constants::Action::LIST, params))
       end
     end
   end
